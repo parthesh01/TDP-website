a2dff76327d861e16c454dca6f6a158f84a9cc70
NIGGERTRON\Parthesh <parthesh01@gmail.com>
2025-04-14 17:13:12 +0530

    deployment error fixed 2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -23,8 +23,7 @@ export default defineConfig({
       },
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          // Add other vendor chunks as needed
+          vendor: ['react', 'react-dom']
         }
       }
     }
